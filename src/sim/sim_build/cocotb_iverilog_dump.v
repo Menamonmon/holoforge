481bf695c6766cd7cf0a4939fea0845ebf9848c9
Yhawaz <yabi@DHCP-POOL-18-25-23-182.MIT.EDU>
2024-11-14 15:15:14 -0500

    Finished dot product and mult

--- src/sim/sim_build/cocotb_iverilog_dump.v
+++ src/sim/sim_build/cocotb_iverilog_dump.v
@@ -1,6 +1,6 @@
 module cocotb_iverilog_dump();
 initial begin
-    $dumpfile("/Users/yabi/Documents/Schooly_Stuff/6.111/holoforge/src/sim/sim_build/fixed_point_mult.fst");
-    $dumpvars(0, fixed_point_mult);
+    $dumpfile("/Users/yabi/Documents/Schooly_Stuff/6.111/holoforge/src/sim/sim_build/fixed_point_fast_dot.fst");
+    $dumpvars(0, fixed_point_fast_dot);
 end
 endmodule
