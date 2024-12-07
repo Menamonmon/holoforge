0634fefb8b68b0844fe7c055794c9a03c5055e56
Mena <58854232+Menamonmon@users.noreply.github.com>
2024-12-07 10:44:02 -0500

    test stacker works connections to MIG and FIFOs still buggy

--- lab06clone/hdl/read_addr_gen.sv
+++ lab06clone/hdl/read_addr_gen.sv
@@ -13,19 +13,27 @@ module read_addr_gen (
   logic [26:0] read_request_address;
   logic [26:0] read_response_address;
 
-always_comb begin
-    read_request_address_out=read_request_address<<4;
-    last_data=read_response_address==115200-1;
-end
+  always_comb begin
+    read_request_address_out = read_request_address << 4;
+    last_data = read_response_address == 115200 - 1;
+  end
 
-evt_counter#(.MAX_COUNT(115200)) read_req_addr(.clk_in(clk_in),
-                                .rst_in(rst_in),
-                                .evt_in(rdy_read_req && valid_read_req),
-                                .count_out(read_request_address));
+  evt_counter #(
+      .MAX_COUNT(115200)
+  ) read_req_addr (
+      .clk_in(clk_in),
+      .rst_in(rst_in),
+      .evt_in(rdy_read_req && valid_read_req),
+      .count_out(read_request_address)
+  );
 
-evt_counter#(.MAX_COUNT(115200)) read_resp_addr(.clk_in(clk_in),
-                                .rst_in(rst_in),
-                                .evt_in(rdy_read_resp && valid_read_resp),
-                                .count_out(read_response_address));
-    
+  evt_counter #(
+      .MAX_COUNT(115200)
+  ) read_resp_addr (
+      .clk_in(clk_in),
+      .rst_in(rst_in),
+      .evt_in(rdy_read_resp && valid_read_resp),
+      .count_out(read_response_address)
+  );
+
 endmodule
